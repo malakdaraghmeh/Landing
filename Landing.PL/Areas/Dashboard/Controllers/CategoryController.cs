d851c8db64e37cfd08489bb284f210b0f85ae348
malakdaraghmeh <malakdaraghmeh23@gmail.com>
2024-10-05 02:06:09 +0300

    Add project files.

---
+++ Landing.PL/Areas/Dashboard/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Landing.PL.Areas.Dashboard.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
